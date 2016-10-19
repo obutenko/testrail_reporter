cc698e086c30db6fee5b916029c51d6cf8ec8234
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-10-19 15:39:11 +0300

    Bump version: 0.6.0 -> 0.7.0 !minor

--- testrail_reporter/__init__.py
+++ testrail_reporter/__init__.py
@@ -1,7 +1,7 @@
 from testrail_reporter.reporter import Reporter
 from testrail_reporter.utils import CaseMapper
 
-__VERSION__ = '0.6.0'
+__VERSION__ = '0.7.0'
 
 
 __all__ = ['CaseMapper', 'Reporter', '__version__']
